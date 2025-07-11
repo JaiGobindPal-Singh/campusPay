51c55cbbda7135fd8f1c2c6968bba8fcfb83d61c
Jai Gobind Pal Singh <jai.gobind.pal@gmail.com>
2025-07-11 19:35:57 +0530

    mend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:"http://localhost:3000",
+        target:"https://campuspay-hlgr.onrender.com/",
         changeOrigin: true,
         secure:false,
         configure: (proxy) => {
